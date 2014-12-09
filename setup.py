578c0a708ea746f9cd4a3a9d179a4d409168f381
Ben Farr <farr@uchicago.edu>
2014-12-08 20:10:30 -0600

    add basic setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kombine',
+      version='1.0',
+      description='An embarrassingly parallel, kernel-density-based ensemble sampler',
+      author='Ben Farr',
+      author_email='farr@uchicago.edu',
+      url='https://github.com/bfarr/kombine',
+      packages=['kombine'],
+     )
