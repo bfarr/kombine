a112885611a23a87acb03e3349177da038432bd5
Ben Farr <farr@uchicago.edu>
2014-12-15 19:02:13 -0600

    use setuptools if available

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+try:
+    from setuptools import setup
 
-setup(name='kombine',
-      version='1.0',
-      description='An embarrassingly parallel, kernel-density-based ensemble sampler',
-      author='Ben Farr',
-      author_email='farr@uchicago.edu',
-      url='https://github.com/bfarr/kombine',
-      packages=['kombine'],
-     )
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='kombine',
+    version='1.0',
+    description='An embarrassingly parallel, kernel-density-based\
+                 ensemble sampler',
+    author='Ben Farr',
+    author_email='farr@uchicago.edu',
+    url='https://github.com/bfarr/kombine',
+    include_package_data=True,
+    packages=['kombine'],
+    install_requires=['numpy', 'scipy'],
+)
