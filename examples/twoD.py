d97ce07d53a41f855f276c169a02629ea4410211
Ben Farr <farr@uchicago.edu>
2014-12-15 19:06:33 -0600

    add an image and 2D demonstration

---
+++ examples/twoD.py
@@ -0,0 +1,66 @@
+"""
+A 2D example.
+"""
+
+import kombine
+
+import numpy as np
+from scipy.misc import imread
+from scipy.ndimage.filters import median_filter
+from scipy.interpolate import RectBivariateSpline
+
+from matplotlib import animation as mpl_animation
+
+
+# Load up the image (greyscale) and filter to soften it up
+img = median_filter(imread('./kombine.png', flatten=True), 5)
+
+# Convert 'ij' indexing to 'xy' coordinates
+img = np.flipud(img).T
+lower_left = np.array([0., 0.])
+upper_right = img.shape
+
+# Construct a spline interpolant to use as a target
+x = np.arange(lower_left[0], upper_right[0], 1)
+y = np.arange(lower_left[1], upper_right[1], 1)
+f = RectBivariateSpline(x, y, img)
+
+
+# Use a uniform (bounded) prior
+def lnprior(X):
+    res = -np.inf*np.ones(len(X))
+    sel = (X[:, 0] > lower_left[0]) & (X[:, 0] < upper_right[0])
+    sel = sel & (X[:, 1] > lower_left[1]) & (X[:, 1] < upper_right[1])
+    res[sel] = np.ones(np.sum(sel))
+    return res
+
+# Use a softened version of the interpolant as a likelihood
+lnlike = lambda X: -3.5*np.log(f(X[:, 0], X[:, 1], grid=False))
+
+
+# Construct a pool if multiprocessing is available
+try:
+    import multiprocessing as mp
+    pool = mp.Pool()
+
+except ImportError:
+    pool = None
+
+
+# Initially distribute the ensemble across the prior
+nwalkers = 1000
+dim = 2
+p = np.random.ranf(size=(nwalkers, dim))
+p = (upper_right - lower_left) * p + lower_left
+
+sampler = kombine.Sampler(nwalkers, dim, lnprior, lnlike, pool=pool)
+
+# Sample for a bit
+p, prior, like, q = sampler.sample(p, iterations=200)
+
+# Animate the ensemble's progress
+anim = sampler.animate()
+
+# Write the animation to file
+writer = mpl_animation.writers['ffmpeg'](fps=30, bitrate=20000)
+anim.save("kombine.mp4", writer=writer)
