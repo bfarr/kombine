22f5c7334eb01f6aaf1264ce368714e76bd2bcad
Duncan Macleod <duncan.macleod@ligo.org>
2018-03-06 16:46:01 +0000

    setup.py: added classifiers

--- setup.py
+++ setup.py
@@ -26,4 +26,17 @@ setup(
     include_package_data=True,
     packages=['kombine'],
     install_requires=['numpy', 'scipy'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
