522979e5127cb974012fb087b1ad34c24fc85590
Duncan Macleod <duncan.macleod@ligo.org>
2018-03-08 16:01:32 +0000

    setup.py: added python_requires classifier

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     include_package_data=True,
     packages=['kombine'],
     install_requires=['numpy', 'scipy'],
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
